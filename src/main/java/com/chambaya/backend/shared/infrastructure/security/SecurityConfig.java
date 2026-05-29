ac8ead90d4a5cbb3bdf252ba195f80906e417e84
Sevas04 <U202111041@upc.edu.pe>
2026-05-29 18:41:23 -0500

    feat: protect endopoints by user roles

--- src/main/java/com/chambaya/backend/shared/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/chambaya/backend/shared/infrastructure/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.chambaya.backend.shared.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import com.chambaya.backend.iam.infrastructure.security.JwtAuthenticationFilter;
@@ -29,7 +30,18 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
                         ).permitAll()
-                        .requestMatchers("/api/v1/**").permitAll()
+                        .requestMatchers("/api/v1/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
+
+                        .requestMatchers(HttpMethod.POST, "/api/v1/jobs").hasRole("CONTRATANTE")
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/jobs/**").hasRole("CONTRATANTE")
+
+                        .requestMatchers(HttpMethod.POST, "/api/v1/enrollments").hasRole("CHAMBEADOR")
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/enrollments/**").authenticated()
+
+                        .requestMatchers(HttpMethod.POST, "/api/v1/reviews").authenticated()
+
+                        .requestMatchers(HttpMethod.GET, "/api/v1/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
